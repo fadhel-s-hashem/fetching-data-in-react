e7d84902ae4cd44196d14e6168bdfde8df565076
Fadhel <fadhel.s.hashem@gmail.com>
2026-07-29 10:28:16 +0300

    useEffect

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react"
+
 const Count = () => {
+
+    const [count, setCount] = useState(0)
+    const [toggle, setToggle] = useState(false)
+
+
+    useEffect(() => {
+        const handleClick =() => {
+        setCount(count + 1)
+        console.log(count)
+    }
+    handleClick()
+
+    }, []) 
+    // [] you need this empty array so it runs one time
+
+    
 
 return(
 
     <div>
         <h1> Count.jsx</h1>
+        <p> {count}</p>
+        {/* <button onClick={handleClick}> +1</button> */}
 
     </div>
 )

932e8a91b4eb5ccb455cdfefab9d3fb4dfafac96
Fadhel <fadhel.s.hashem@gmail.com>
2026-07-29 10:11:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Count from './components/Count'
+
+const App = () => {
+  return (
+  <div>
+  <h1>Fetching Data in React</h1>
+
+    <Count/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Count.jsx
@@ -0,0 +1,12 @@
+const Count = () => {
+
+return(
+
+    <div>
+        <h1> Count.jsx</h1>
+
+    </div>
+)
+}
+
+export default Count

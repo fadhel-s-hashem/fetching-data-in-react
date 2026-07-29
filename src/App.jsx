79766eb9668daced3cca6968e466bca648ef598b
Fadhel <fadhel.s.hashem@gmail.com>
2026-07-29 14:24:26 +0300

    show API array in browser

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,19 @@ import * as studentService from './services/students'
 
 const App = () => {
   // use useEffect + our student service to get all the students
-  useEffect(() => {}, [])
   // save all of the student to some state
-
   // use the state and map through the students
+
+  useEffect(() => {
+    const fetchAllStudents = async () => {
+       const studentsData= studentService.index()
+       console.log(studentsData) // to see the API array in browser console
+    }
+
+    fetchAllStudents()
+  
+  }, [])
+
   return (
   <div>
   <h1>Fetching Data in React</h1>

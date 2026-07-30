cc942536c2240628efa1e830d6f07c2d28bd744f
Fadhel <fadhel.s.hashem@gmail.com>
2026-07-30 10:30:18 +0300

    find each student by id

--- src/pages/StudentDetails.jsx
+++ src/pages/StudentDetails.jsx
@@ -1,9 +1,18 @@
+import { useParams } from "react-router"
 
-const StudentDetails = () => {
+const StudentDetails = (props) => {
+
+    const {studentId} = useParams()
+    const student = props.students.find((student) => {
+        return student.id === Number(studentId)
+    })
 
     return(
         <main>
-            this detail page
+            <h3>hello {student.name}</h3> 
+            <p> favorite emoji:  {student.favoriteEmoji}</p>
+                <p> favorite food: {student.favoriteFood}</p>
+
         </main>
 
     )
